6aae146cc528a2503162c7adc9f6d9a260b03eee
HevertSousa <hevert.sousa@outlook.com>
2022-09-02 16:52:15 -0300

    Iniciando Nova Linguagem

---
+++ mavenproject1/src/main/java/Aula_5/Pessoa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula_5;
+
+/**
+ *
+ * @author Hevert.Sousa
+ */
+public class Pessoa {
+    // atributos ou características
+       public String nome;
+       public int idade, dataNasc;
+        
+    // comportamentos ou funções (métodos) 
+        public int calc(int anoN){
+            idade = 2022 - anoN;
+            return idade;
+        }
+}
